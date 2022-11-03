00c42ffa3524974bbfb5af37fa505bd381bf90e7
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-11-03 19:48:13 +0530

    text content component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../Header";
+import TextContent from "../TextContent";
 import "./index.scss";
 
 const App = () => {
   return (
     <section>
       <Header></Header>
-      {/* <TextContent /> */}
+      <TextContent />
       {/* <Cards></Cards> */}
     </section>
   );
---
+++ src/components/TextContent/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.scss";
+
+export default function TextContent() {
+  return (
+    <section className="text-content-container">
+      <p className="text-content-container-title">Stays in Finland</p>
+      <p className="text-content-container-stay-count">12+ stays</p>
+    </section>
+  );
+}
