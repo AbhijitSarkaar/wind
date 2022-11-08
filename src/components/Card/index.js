767f03e19578c673d539b7a36b5b392052cb1246
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2022-11-08 12:45:34 +0530

    card component and render list of cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import "./index.scss";
 
 export default function Card(props) {
@@ -11,7 +13,18 @@ export default function Card(props) {
           className="card-container-image-container-image"
         />
       </div>
-      <div className="card-container-text"></div>
+      <div className="card-container-text">
+        {data.superHost && (
+          <div className="card-container-text-super-host-tag">Super Host</div>
+        )}
+        <div className="card-container-text-space">{data.type}</div>
+        <div className="card-container-text-bed-count">{data.beds} beds</div>
+        <div className="card-container-text-rating-text">
+          <FontAwesomeIcon icon={faStar} color={"#EB5757"} />
+          <div>{data.rating}</div>
+        </div>
+      </div>
+      <div className="card-container-title">{data.title}</div>
     </section>
   );
 }
--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -4,24 +4,11 @@ import "./index.scss";
 
 export default function CardList(props) {
   const { data } = props;
-  const item = {
-    city: "Helsinki",
-    country: "Finland",
-    superHost: false,
-    title: "Stylist apartment in center of the city",
-    rating: 4.4,
-    maxGuests: 3,
-    type: "Entire apartment",
-    beds: 2,
-    photo:
-      "https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80",
-  };
   return (
     <section className="card-list-container">
-      <Card data={item} />
-      {/* {data.map((item) => (
+      {data.map((item) => (
         <Card data={item} key={item.photo} />
-      ))} */}
+      ))}
     </section>
   );
 }
